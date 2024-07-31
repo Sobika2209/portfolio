c0855595546fa77ef5ac32cf13f5bef8d6712501
Sobika R <146466870+Sobika2209@users.noreply.github.com>
2024-07-31 22:52:29 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+var typed = new Typed('#element', {
+    strings: ['<i>Web Developer</i>', ' <i>Team Worker</i>', ' <i>Java Developer</i>'],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true
+});
